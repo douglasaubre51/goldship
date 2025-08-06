e86aa05112380dffdf2abc3c377df89369690cb7
allen <boseallen192@gmail.com>
2025-08-06 13:50:28 +0530

    added main page

--- App.tsx
+++ App.tsx
@@ -1,32 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { Text, View } from "react-native";
+import { Text, View, TextInput, Button } from "react-native";
+import Toast from "react-native-toast-message"
 
 
 function App() {
 
+  const showToastMessage = () => {
+    Toast.show({
+      type: 'success',
+      text1: 'logged in!',
+      text2: 'welcome',
+      position: 'bottom',
+    })
+  }
+
   return (
-    <View style={{
-      margin: 20,
-    }}>
-      <Text style={{
-        fontSize: 32,
-      }}>
-        hello, i am back for my throne
-      </Text>
-      <Text style={{
-        marginTop: 10,
-        fontSize: 20
-      }}
-      >
-        now this world shall know pain
-      </Text>
-    </View>
+
+    <>
+      {/* main layout */}
+      <View style={{
+        margin: 20,
+        flexDirection: "column",
+        gap: 24
+      }
+      }>
+        {/* header */}
+        <Text style={{
+          fontSize: 32,
+        }}>
+          GoldShip
+        </Text>
+
+        {/* name group */}
+        <View style={{
+          flexDirection: "column",
+          gap: 6
+        }}>
+          <Text style={{
+            marginTop: 10,
+            fontSize: 20
+          }}
+          >
+            username
+          </Text>
+          <TextInput style={{
+            borderColor: "grey",
+            borderWidth: 1,
+            borderRadius: 8
+          }} />
+        </View>
+
+        {/* password group */}
+        <View style={{
+          flexDirection: "column",
+          gap: 6
+        }}>
+          <Text style={{
+            marginTop: 10,
+            fontSize: 20
+          }}
+          >
+            password
+          </Text>
+          <TextInput style={{
+            borderColor: "grey",
+            borderWidth: 1,
+            borderRadius: 8
+          }} />
+
+        </View>
+        {/* button */}
+        <Button
+          title="login"
+          onPress={showToastMessage}
+        />
+      </View >
+      <Toast />
+    </>
   );
 }
 
